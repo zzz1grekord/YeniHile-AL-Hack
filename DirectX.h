83264125ef59a118c226d31e145fece2341fde02
zzz1grekord <44650837+zzz1grekord@users.noreply.github.com>
2019-05-02 09:21:25 +0300

    Update DirectX.h

--- DirectX.h
+++ DirectX.h
@@ -2,10 +2,10 @@
 class DirectX
 {
 public:
-	IDXGISwapChain* m_swapChain; //交换链对象
-	ID3D11Device* m_device;  //设备对象
-	ID3D11DeviceContext* m_deviceContext; //设备上下文对象
-	ID3D11RenderTargetView* m_renderTargetView; //渲染目标视图
+	IDXGISwapChain* m_swapChain; //Swap chain objects
+	ID3D11Device* m_device;  //Device objects
+	ID3D11DeviceContext* m_deviceContext; //Device context object
+	ID3D11RenderTargetView* m_renderTargetView; //Render target view
 
 	static DirectX* __fastcall GetInstance();
 	char* __fastcall GetCardDescription();
@@ -19,9 +19,9 @@ public:
 
 private:
 	DirectX();
-	bool m_vsync_enabled; //是否启用垂直同步
-	int m_videoCardMemory; //显存大小
-	char m_videoCardDescription[128]; //显卡名字
+	bool m_vsync_enabled; //Whether Vertical Sync is enabled
+	int m_videoCardMemory; //Memory size
+	char m_videoCardDescription[128]; //Video card name
 
 	static DirectX* m_pInstance;
 };
